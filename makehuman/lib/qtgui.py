5128e124327a383a18a54afc762919e6501affcb
Aranuvir <Aranuvir@github>
2017-07-29 17:38:12 +0200

    remove commandline option for splash screen

--- makehuman/lib/qtgui.py
+++ makehuman/lib/qtgui.py
@@ -1505,10 +1505,7 @@ class FileEntryView(QtGui.QWidget, Widget):
 
 class SplashScreen(QtGui.QSplashScreen):
     def __init__(self, image, version=""):
-        if G.args.get('splashontop') == True:
-            super(SplashScreen, self).__init__(G.app.mainwin, getPixmap(image), QtCore.Qt.WindowStaysOnTopHint)
-        else:
-            super(SplashScreen, self).__init__(G.app.mainwin, getPixmap(image))
+        super(SplashScreen, self).__init__(G.app.mainwin, getPixmap(image))
         self._stdout = sys.stdout
         self.messageRect = QtCore.QRect(354, 531, 432, 41)
         self.messageAlignment = QtCore.Qt.AlignLeft
